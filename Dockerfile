020f6019eb524fbadf9d8328080a2cefe8054a46
Ryan Bott <tmp8496@gmail.com>
2024-08-27 12:07:53 -0400

    removed redis added sqlite for sqmodel refactor. Basic counter functionality restored.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM python:3.12.4-alpine3.20
 WORKDIR /card-golf
 ENV PATH="/root/.local/bin:$PATH"
-RUN apk add curl
+RUN apk add --no-cache sqlite curl
 RUN curl -sSL https://install.python-poetry.org | python3 -
 COPY ./entrypoint.sh /card-golf/
 RUN chmod +x entrypoint.sh
--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-import redis
+from sqlmodel import Field, Session, SQLModel, create_engine
 
-r = redis.Redis(host='redis', port=6379, db=0)
+class Counter(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    counter: int | None = None
+
+sqlite_file_name = "golf.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def initialize_counter():
+      counter = Counter(counter=0)
+      with Session(engine) as session:
+            session.add(counter)
+            session.commit()
+
+def increment_counter():
+      with Session(engine) as session:
+            counter = session.get(Counter, 1)
+            counter.counter += 1
+            session.add(counter)
+            session.commit()
+
+def get_counter():
+      with Session(engine) as session:
+            counter = session.get(Counter, 1)
+            return counter.counter
 
 app = FastAPI()
-
 app.mount("/golf", StaticFiles(directory="golf/", html=True), name="golf")
+create_db_and_tables()
+initialize_counter()
 
+counter = Counter(counter=0)
 
 @app.get("/counter")
 async def hello():
-      r.incr('counter')
-      return f"{r.get('counter')}"
+      increment_counter()
+      return f"{get_counter()}"
 
 @app.get('/')
 async def front():
